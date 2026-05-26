c6024fb965a12c7d51129b8a9c3d0810eb0f245c
dhrumit <dhrumittechrabbit@gmail.com>
2026-05-26 11:59:51 +0530

    feat: load auth state from AsyncStorage on app boot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
 import toastConfig from "@/components/CustomeTost";
+import { useAuthStore } from "@/store/useAuthStore";
 import { Slot } from "expo-router";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
+  useEffect(() => {
+    useAuthStore.getState().loadFromStorage();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Slot />
-      {/* <StatusBar barStyle={"default"} /> */}
       <Toast position="bottom" config={toastConfig} />
     </GestureHandlerRootView>
   );
